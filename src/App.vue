59bb67e8998cf752ce9b8bdc436474f2c8fc2465
VincenzoBorriello89 <vincborr89@gmail.com>
2022-04-28 13:08:15 +0200

    creazione file e primi comp

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="app">
+    <HeaderComp />
+    <MainComp />
+    <FooterComp />
+  </div>
+</template>
+
+<script>
+import HeaderComp from "./components/HeaderComp.vue";
+import MainComp from "./components/MainComp.vue";
+import FooterComp from "./components/FooterComp.vue";
+
+export default {
+  name: "App",
+  components: {
+    HeaderComp,
+    MainComp,
+    FooterComp,
+  },
+};
+</script>
+
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
